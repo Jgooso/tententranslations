528b953b7acd49e1eeb395ee91d18ad96ce7d70e
Jgooso <joshgoodman0220@gmail.com>
2022-06-13 14:19:37 -0700

    changed cors

--- flaskbackend/app.py
+++ flaskbackend/app.py
@@ -9,7 +9,7 @@ from scripts import download
 def create_app(config_objct=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config_objct)
-    CORS(app, resources={r'/*':{'origins':'*'}})
+    CORS(app, resources={r'*':{'origins':'*'}})
     return app
 
 
