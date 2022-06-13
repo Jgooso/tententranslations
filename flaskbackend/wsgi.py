e7d9b38a55df4c8728c5527ffebd305d522e2a55
Jgooso <joshgoodman0220@gmail.com>
2022-06-13 14:24:14 -0700

    attempted bug fix

--- flaskbackend/wsgi.py
+++ flaskbackend/wsgi.py
@@ -19,6 +19,7 @@ def upload():
 @app.after_request
 def add_header(response):
     response.access_control_allow_origin = "*"
+    response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
