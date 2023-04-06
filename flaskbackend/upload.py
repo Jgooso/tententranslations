fc1e585c3d663acf2f2a6383622b8fd8e717954a
Jgooso <joshgoodman0220@gmail.com>
2023-04-06 15:12:47 -0500

    aaa

---
+++ flaskbackend/upload.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from settings import Config
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+config = Config.SQL_SETTINGS
+def schedule_upload():#change permissions for viewing of novels
+    noveldb = mysql.connector.connect(**config)
+    novelcursor = noveldb.cursor(buffered=True)
+    chapter_sql =  """
+                   UPDATE chapters SET chapteractive = 1 WHERE DATE(uploaddate) = CURDATE() AND HOUR(uploaddate) = HOUR(NOW());
+                   UPDATE novels INNER JOIN chapters on novels.novelid = chapters.novelid SET lastupload = NOW() WHERE DATE(uploaddate) = CURDATE() AND HOUR(uploaddate) = HOUR(NOW());
+                   """
+    #upload_bot(novelcursor=novelcursor,novel)
+    noveldb.commit()
+    noveldb.close()
+
+def upload_bot(novelcursor,novel):
+    novelcursor.execute("SELECT novels.title,novels.novelid,chapters.chapternumber FROM chapters INNER JOIN novels ON chapters.novelid = novels.id WHERE chapteractive = 1 and novels.id = %s ORDER BY chapternumber+0 DESC LIMIT 1",(novel,))
+    chapter = novelcursor.fetchone()
+    novel_title = chapter[0]
+    link = 'https://tententranslation/novels/'+str(chapter[1])+'/'+str(chapter[2])
+    #print(link)
+    #print(novel_title)
+    chapter_number = chapter[2]
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.get('https://www.novelupdates.com')
+    #GET TO LOGIN PAGE
+    browser.find_element(by=By.LINK_TEXT, value='Login').click()
+    browser.find_element(by=By.ID, value='user_login').send_keys("jgooso")
+    browser.find_element(by=By.ID, value = 'user_pass').send_keys("testingpassword")
+    browser.find_element(by=By.ID, value = 'wp-submit').click()
+    #GET TO UPLOAD PAGE
+    browser.find_elements(by=By.ID, value='menu_right_item')[1].click()
+    browser.find_element(by=By.LINK_TEXT, value='User Profile').click()
+    browser.find_element(by=By.CLASS_NAME,value = 'ptool').click()
+    #TYPE DATA INTO BOXES
+    browser.find_element(by=By.ID, value = 'title_change_100').send_keys(novel_title)
+    browser.find_element(by=By.ID, value = 'arrelease').send_keys(chapter_number)
+    browser.find_element(by=By.ID,value = 'arlink').send_keys(link)
+    browser.find_element(by=By.ID,value='group_change_100').send_keys('tententranslations')
+    #SUBMIT
+
+    #CLOSE BOT
+    browser.close()
